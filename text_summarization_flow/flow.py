0a4ec4a2808f451d3da73267d71f49474acbd6e3
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-07-16 09:37:38 +0100

    Added pyi file for better typing + pyrightconfig.json

--- text_summarization_flow/flow.py
+++ text_summarization_flow/flow.py
@@ -1,40 +1,40 @@
 from os import PathLike
 from pathlib import Path
 
-from pocketflow import Flow, Node
+from pocketflow import BatchNode, Flow, Node
 from utils.llm_utils import call_llm
 
 
-class LoadFile(Node):
+class LoadFile(Node[PathLike, str, None]):
     def prep(self, shared):
         return shared["filepath"]
 
-    def exec(self, filepath: PathLike):
-        filepath = Path(filepath)
-        return open(filepath, "r").read()
+    def exec(self, prep_res: PathLike):
+        prep_res = Path(prep_res)
+        return open(prep_res, "r").read()
 
-    def post(self, shared, _, text):
-        shared["raw"] = text
+    def post(self, shared, prep_res, exec_res):
+        shared["raw"] = exec_res
 
 
-class Summarize(Node):
+class Summarize(Node[str, str, None]):
     def prep(self, shared):
         return shared["raw"]
 
-    def exec(self, text: str):
-        prompt = f"Summaryize in exactly 10 words: {text}"
+    def exec(self, prep_res: str):
+        prompt = f"Summaryize in exactly 10 words: {prep_res}"
         return call_llm(prompt)
 
-    def post(self, shared, _, summary):
-        shared["summary"] = summary
+    def post(self, shared, prep_res, exec_res):
+        shared["summary"] = exec_res
 
 
-class PrintResult(Node):
+class PrintResult(Node[str, None, None]):
     def prep(self, shared):
         return shared["summary"]
 
-    def exec(self, summary: str):
-        print("== Summary ==\n", summary)
+    def exec(self, prep_res: str):
+        print("== Summary ==\n", prep_res)
 
 
 # Wire up the flow
